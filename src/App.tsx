d15e3af2cfa377f82ee86d3be4252c487dd6f1b8
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-06 17:37:16 +0700

    feat(social): made clan ui dashboard page with randomized dummy UUID and API library for fetching to internal API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Reading from './pages/reading/Reading.js';
+import ClanDashboard from './pages/social/ClanDashboard.js';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/readings" element={<Reading />} />
+                <Route path="/clan" element={<ClanDashboard />} />
             </Routes>
         </Router>
     );
