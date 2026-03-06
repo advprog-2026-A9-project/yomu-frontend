9493dfcaf14e07f36a8d76d0c04294256e969831
piesfication <muhammad.rafi42@ui.ac.id>
2026-03-06 16:54:50 +0700

    [green] feat(auth): implement LoginForm

--- src/test/auth/LoginForm.test.tsx
+++ src/test/auth/LoginForm.test.tsx
@@ -3,7 +3,6 @@ import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import LoginForm from '../../components/auth/LoginForm';
 
-// Mock authAPI
 vi.mock('../../services/authAPI', () => ({
     loginUser: vi.fn(),
 }));
@@ -14,19 +13,19 @@ describe('LoginForm', () => {
         render(<LoginForm />);
         expect(screen.getByPlaceholderText('Username / Email / Nomor HP')).toBeInTheDocument();
         expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
-        expect(screen.getByText('Login')).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
     });
 
     test('menampilkan error jika identifier kosong', async () => {
         render(<LoginForm />);
-        await userEvent.click(screen.getByText('Login'));
+        await userEvent.click(screen.getByRole('button', { name: 'Login' }));
         expect(screen.getByText('Username/Email/HP harus diisi')).toBeInTheDocument();
     });
 
     test('menampilkan error jika password kosong', async () => {
         render(<LoginForm />);
         await userEvent.type(screen.getByPlaceholderText('Username / Email / Nomor HP'), 'mizukitest');
-        await userEvent.click(screen.getByText('Login'));
+        await userEvent.click(screen.getByRole('button', { name: 'Login' }));
         expect(screen.getByText('Password harus diisi')).toBeInTheDocument();
     });
 });
